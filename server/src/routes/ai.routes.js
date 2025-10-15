0edfb7fb63ed1bc1c3fa6ff612adb05572a0b37b
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-10-15 17:29:06 +0530

    feat(route) :  add image and blogTitle generation routes

--- server/src/routes/ai.routes.js
+++ server/src/routes/ai.routes.js
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import { Route } from "express";
 import { userAccessMiddleware } from "../middlewares/auth.middleware.js";
-import { generateArticle } from "../controllers/ai.controller.js";
+import { generateArticle, generateBlogTitle, generateImage } from "../controllers/ai.controller.js";
 
 const router = Router();
 
 router.post('/generate-article' , userAccessMiddleware , generateArticle);
+router.post('/generate-blogTitle' , userAccessMiddleware , generateBlogTitle);
+router.post('/generate-image' , userAccessMiddleware , generateImage);
 
 export default router;
