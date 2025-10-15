3f16ee5409c85b56b626c90eb22d7a4229919a27
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-10-15 20:07:59 +0530

    feat(route) : add bg remove  and object remove routes

--- server/src/routes/ai.routes.js
+++ server/src/routes/ai.routes.js
@@ -1,12 +1,15 @@
 import express, { Router } from "express";
 import { Route } from "express";
 import { userAccessMiddleware } from "../middlewares/auth.middleware.js";
-import { generateArticle, generateBlogTitle, generateImage } from "../controllers/ai.controller.js";
+import { generateArticle, generateBlogTitle, generateImage, removeBackground, removeObject } from "../controllers/ai.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.post('/generate-article' , userAccessMiddleware , generateArticle);
 router.post('/generate-blogTitle' , userAccessMiddleware , generateBlogTitle);
 router.post('/generate-image' , userAccessMiddleware , generateImage);
+router.post('/remove-background' ,userAccessMiddleware , upload.single('image') , removeBackground);
+router.post('/remove-object' , userAccessMiddleware , upload.single('image') , removeObject);
 
 export default router;
