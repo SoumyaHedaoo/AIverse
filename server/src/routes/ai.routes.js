51a94249e150fa42982e0df6d6df508640164d7f
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-10-16 16:02:33 +0530

    feat(route) : add resume revies functionality route

--- server/src/routes/ai.routes.js
+++ server/src/routes/ai.routes.js
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 import { Route } from "express";
 import { userAccessMiddleware } from "../middlewares/auth.middleware.js";
-import { generateArticle, generateBlogTitle, generateImage, removeBackground, removeObject } from "../controllers/ai.controller.js";
+import { generateArticle, generateBlogTitle, generateImage, removeBackground, removeObject, resumeReview } from "../controllers/ai.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -11,5 +11,6 @@ router.post('/generate-blogTitle' , userAccessMiddleware , generateBlogTitle);
 router.post('/generate-image' , userAccessMiddleware , generateImage);
 router.post('/remove-background' ,userAccessMiddleware , upload.single('image') , removeBackground);
 router.post('/remove-object' , userAccessMiddleware , upload.single('image') , removeObject);
+router.post('/remove-object' , userAccessMiddleware , upload.single('resumeFile') , resumeReview);
 
 export default router;
